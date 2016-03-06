3eaa638cbf7ee661ee866a8b9a8c5537e441bc7f
b27 <b27@debian>
2016-03-06 21:38:17 +0100

    Modified copy1 function, added implementation of simple cat.

---
+++ class_2/cat1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int write_to(int fd_from)
+{
+    char buf[514];
+    ssize_t nread = 0;
+
+    while ((nread = read(fd_from, buf, sizeof(buf))) > 0) {
+        if (fprintf(stdout, buf) < 0) {
+            fprintf(stderr, "Error while writing file. Err number: %d", errno);
+            goto cleanup;
+        }
+    }
+    if (nread == -1) {
+        fprintf(stderr, "Error while reading file. Err number: %d", errno);
+        goto cleanup;
+    }
+
+    return 0;
+
+    cleanup:
+        return -1;
+}
+
+int show_file(char *file_name)
+{
+    int fd = -1;
+
+    if ((fd = open(file_name, O_RDONLY)) == -1) {
+        fprintf(stderr, "Error while opening file. Err number: %d", errno);
+        goto cleanup;
+    }
+
+    if (write_to(fd) == -1) {
+        goto cleanup;
+    }
+
+    close(fd);
+    return 0;
+
+    cleanup:
+        if (fd != -1)
+            close(fd);
+        return -1;
+}
+
+int show_without_file()
+{
+    if (write_to(STDIN_FILENO) == -1) {
+        goto cleanup;
+    }
+
+    return 0;
+
+    cleanup:
+        return -1;
+}
+
+int main(int argc, char **argv)
+{
+
+    if (argc > 2) {
+        fprintf(stderr, "Bad args!");
+        return 1;
+    }
+
+    if (argc == 2) {
+        return show_file(argv[1]);
+    } else {
+        return show_without_file();
+    }
+}
--- class_2/copy1.c
+++ class_2/copy1.c
@@ -18,6 +18,42 @@
 
 #define BUFSIZE 512
 
+void manage_open_err() {
+    switch (errno) {
+        case EACCES:
+            fprintf(stderr, "From man: The  requested  access  to the file is not allowed, "
+                    "or search permission is denied for one of the directories in "
+                    "the path prefix of\n"
+                    "pathname, or the file did not exist yet and write "
+                    "access to the parent directory is not allowed.");
+            break;
+
+        case ENOENT:
+            fprintf(stderr, "O_CREAT is not set and the named file does not exist.  "
+                    "Or, a directory component in pathname does not exist or is a  "
+                    "dangling  sym‐\n"
+                    "bolic link. \n");
+            break;
+        default:
+            fprintf(stderr, "Some error occurred!");
+    }
+}
+
+ssize_t writeall(int fd, const void *buf, size_t nbyte) {
+    ssize_t nwritten = 0, n;
+
+    do {
+        if ((n = write(fd, &((const char *) buf)[nwritten], nbyte - nwritten)) == -1) {
+            if (errno == EINTR)
+                continue;
+            else
+                return -1;
+        }
+        nwritten += n;
+    } while (nwritten < nbyte);
+    return nwritten;
+}
+
 void copy(char *from, char *to)  /* has a bug */
 {
     int fromfd = -1, tofd = -1;
@@ -25,38 +61,33 @@ void copy(char *from, char *to)  /* has a bug */
     char buf[BUFSIZE];
 
     if ((fromfd = open(from, O_RDONLY)) == -1) {
-        switch(errno) {
-            case EACCES:
-                fprintf(stderr, "From man: The  requested  access  to the file is not allowed, "
-                        "or search permission is denied for one of the directories in "
-                        "the path prefix of\n"
-                        "pathname, or the file did not exist yet and write "
-                        "access to the parent directory is not allowed.");
-                break;
-
-            case ENOENT:
-                fprintf(stderr, "O_CREAT is not set and the named file does not exist.  "
-                        "Or, a directory component in pathname does not exist or is a  "
-                        "dangling  sym‐\n"
-                        "bolic link. \n");
-                break;
-            default:
-                fprintf(stderr, "Some error occurred!");
-        }
-
-        return;
+        manage_open_err();
+        goto cleanup;
     }
 
 
-    tofd = open(to, O_WRONLY | O_CREAT | O_TRUNC,
-                S_IRUSR | S_IWUSR);
+    if ((tofd = open(to, O_WRONLY | O_CREAT | O_TRUNC,
+                     S_IRUSR | S_IWUSR)) == -1) {
+        manage_open_err();
+        goto cleanup;
+    }
 
     while ((nread = read(fromfd, buf, sizeof(buf))) > 0) {
-        write(tofd, buf, nread);
+        if (write(tofd, buf, nread) == -1) {
+            fprintf(stderr, "Error during writing.");
+            goto cleanup;
+        }
+    }
+    if (nread == -1) {
+        fprintf(stderr, "Error during reading file.");
+        goto cleanup;
     }
 
-    close(fromfd);
-    close(tofd);
+    cleanup:
+    if (fromfd != -1)
+        close(fromfd);
+    if (tofd != -1)
+        close(tofd);
 
     return;
 }
